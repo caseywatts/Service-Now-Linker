8bf62251eb69b7145ce33c520833404dff8b270f
caseywatts <casey.s.watts@gmail.com>
2012-09-12 22:39:08 -0400

    created search for inc with dashes, but not replacing the link correctly yet

--- core.js
+++ core.js
@@ -62,6 +62,9 @@
     findAndReplace('\\b(INC\\d{7})\\b',
                    '<a href=\"https://yale.service-now.com/textsearch.do?sysparm_no_redirect=true&sysparm_search=$&\" target=\"_blank\">$&</a>'
     );
+    //findAndReplace('\\b(INC-\\d{3}-\\d{4})\\b',
+                   //'<a href=\"https://yale.service-now.com/textsearch.do?sysparm_no_redirect=true&sysparm_search=$&\" target=\"_blank\">$&</a>'
+    //);
   }
 
 jQuery(document).ready(function(){
